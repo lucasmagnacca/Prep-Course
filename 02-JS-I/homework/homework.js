f2aae474bf1e125ca059162192b4fe0c03d2d640
Your Name <you@example.com>
2022-03-29 14:20:11 -0300

    v1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -85,7 +85,9 @@ function tienenMismaLongitud(str1, str2) {
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
-  // Tu código:
+  // Tu código: 
+  if (num < 90) {return true}
+  return false 
   
 }
 
