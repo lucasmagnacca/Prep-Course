56eef380646625f58ec03b0bd40b4259d6fffdac
Your Name <you@example.com>
2022-03-29 14:24:44 -0300

    v1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -96,12 +96,15 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (50 < num) {return true
+  }
+  return false 
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  obtenerResto = x % y
   
 }
 
