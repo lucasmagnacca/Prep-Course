bf3eb0a607a0e8b59a686d48ebd3cb13a4eb9bed
Your Name <you@example.com>
2022-03-30 21:32:29 -0300

    as

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -90,7 +90,7 @@ function fizzBuzz(numero) {
   // De lo contrario, devuelve el numero
   if (numero%3 === 0) {return "fizz"}
   if (numero%5 === 0) {return "buzz"}
-  if (numero%3 === 0 & numero%5 === 0) {return "fizzbuzz"}
+  if (numero%15 === 0) {return "fizzbuzz"}
   else {return numero}
 }
 
