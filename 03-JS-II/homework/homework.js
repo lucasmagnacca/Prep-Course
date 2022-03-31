cca7e5ea2109686bfc173f3e89ac226544711020
Your Name <you@example.com>
2022-03-31 09:40:53 -0300

    cam

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -120,7 +120,7 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
  if (numero < 2) {return false}
  if (numero === 2) {return true}
-  for (let i = 2 ; numero > (i-1); i++){
+  for (let i = 2 ; numero > i; i++){
   if (numero% i === 0) {return false} }
   return true
 }
@@ -129,6 +129,8 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if (valor === true)  {return "Soy verdadero"}
+  else if (valor === false) {return "Soy falso"}
 
 }
 
@@ -136,19 +138,26 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  let myArray= []
+  for (let l = 0; l < 11; l++){
+  myArray.push(6*l)}
+  return myArray
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+if (numero > 99 && numero < 1000) {return true}
+return false  
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  do {numero + 5}
+  while (numero < (numero+ 40))
+  console.log(numero)
 }
 
 
