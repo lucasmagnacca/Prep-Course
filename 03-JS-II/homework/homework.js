5026c2a808ef2bf6acf248b1457b71767c2e814c
Your Name <you@example.com>
2022-03-30 21:54:08 -0300

    as

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -101,7 +101,13 @@ function operadoresLogicos(num1, num2, num3) {
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false
+  if (num1 && num2 && num3 === 0) {return "Error"}
+  if (num1 > 0 && num2 && num3) {return "Número 1 es mayor y positivo"} 
+  if (0 > num1 && num2 && num3) {return "Hay negativos"} 
+  if (num3 > num1 && num2) {return num3++} 
+  
+  return false
 }
 
 function esPrimo(numero) {
